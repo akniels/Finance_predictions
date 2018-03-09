15c50592472ce64af275ddbf1a5684f36d6f795a
{akniels} <{andrew.kyle.nielson@gmail.com}>
2018-03-08 20:52:12 -0700

    third_commit

---
+++ obtain and load.py
@@ -0,0 +1,24 @@
+class obtain_and_load():
+    def __init__(self, cur):
+         self.cur = cur
+    def obtain_parse_wiki_snp500(self):
+        ## get time 
+        now = datetime.datetime.utcnow()
+        ## get HTML for the to 
+        response = requests.get(
+        "http://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+        )
+        soup = bs4.BeautifulSoup(response.text)
+      #  symbolslist = soup.select(’table’)[0].select(’tr’)[1:]
+        symbols = []
+        for i, symbol in enumerate(symbolslist):
+            tds = symbol.select(’td’)
+            symbols.append(
+                (
+                    tds[0].select(’a’)[0].text, # Ticker ’stock’,
+                    tds[1].select(’a’)[0].text, # Name
+                        tds[3].text, # Sector
+                        ’USD’, now, now
+                    )
+                )
+        return symbols
