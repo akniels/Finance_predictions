dbfa5f93a86d0e826073cc9067c32324c9fb422a
{akniels} <{andrew.kyle.nielson@gmail.com}>
2018-03-08 21:00:43 -0700

    third commit

--- obtain and load.py
+++ obtain and load.py
@@ -1,3 +1,6 @@
+import datetime
+import requests
+import bs4
 class obtain_and_load():
     def __init__(self, cur):
          self.cur = cur
@@ -9,16 +12,15 @@ class obtain_and_load():
         "http://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
         )
         soup = bs4.BeautifulSoup(response.text)
-      #  symbolslist = soup.select(’table’)[0].select(’tr’)[1:]
+        symbolslist = soup.select('table')[0].select('tr')[1:]
         symbols = []
         for i, symbol in enumerate(symbolslist):
-            tds = symbol.select(’td’)
+            tds = symbol.select('td')
             symbols.append(
                 (
-                    tds[0].select(’a’)[0].text, # Ticker ’stock’,
-                    tds[1].select(’a’)[0].text, # Name
-                        tds[3].text, # Sector
-                        ’USD’, now, now
+                    tds[0].select('a')[0].text, # Ticker ’stock’,
+                    tds[1].select('a')[0].text, # Name
+                        tds[3].text, # Sector ’USD’, now, now
                     )
                 )
         return symbols
